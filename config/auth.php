5048ec70c1bb9664080f0db92a0ae64174e2d8a6
obamwonyi <developer.oduwa@gmail.com>
2022-10-11 09:34:56 +0100

    setup mysql database with an initial migration

--- config/auth.php
+++ config/auth.php
@@ -40,6 +40,10 @@ return [
             'driver' => 'session',
             'provider' => 'users',
         ],
+        'api' => [
+            'driver' => 'passport', 
+            'provider' => 'users',
+        ]
     ],
 
     /*
@@ -63,6 +67,7 @@ return [
         'users' => [
             'driver' => 'eloquent',
             'model' => App\Models\User::class,
+            Laravel\Passport\PassportServiceProvider::class,
         ],
 
         // 'users' => [
