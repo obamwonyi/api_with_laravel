f4b352513a2f84b46c46b830a45b925fa0573c42
obamwonyi <developer.oduwa@gmail.com>
2022-10-13 08:24:17 +0100

    put the user route under a route group (v1)

--- routes/api.php
+++ routes/api.php
@@ -14,11 +14,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
 
-Route::get('/test',function(Request $request)
+Route::middleware("auth:api")->prefix('v1')->group(function()
 {
-    return "Authenticated";
+     Route::get('/user',function(Request $request)
+     {
+        return $request->user();
+     });
 });
